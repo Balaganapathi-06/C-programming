6df5360522cd2554817a36ab832cf36ae26c8602
Balaganapathi-06 <balaganapathi2006@gmail.com>
2024-09-18 16:45:37 +0530

    Add files via upload

---
+++ FACTORIAL.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+ int n,i,j,fact=1;
+ clrscr();
+ printf("enter the no:");
+ scanf("%d",&n);
+ for(i=n;i!=0
+ ;i=i-1)
+ {
+   fact=fact*i;
+ }
+ printf("%d",fact);
+ getch();
+}

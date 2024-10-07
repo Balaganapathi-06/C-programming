b8d62da5a782529fe5945b1e913a61c7f0602b03
Balaganapathi-06 <balaganapathi2006@gmail.com>
2024-10-07 21:38:19 +0530

    Add files via upload

---
+++ GRESUMFN.C
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <conio.h>
+int gresum(int n1,int n2,int n3)
+{
+  int sum;
+  if(n1>n2)
+  {
+    if(n2>n3)
+    return sum=n1+n2;
+    else
+    return sum=n1+n3;
+  }
+  else if(n2>n3)
+  {
+    if(n3>n1)
+    return sum=n2+n3;
+    else
+    return sum=n2+n1;
+  }
+  else
+  {
+    if(n2>n1)
+    return sum=n3+n2;
+    else
+    return sum=n3+n1;
+  }
+}
+void main()
+{
+  int n1,n2,n3,sum;
+  clrscr();
+  printf("Enter the no 1:");
+  scanf("%d",&n1);
+  printf("Enter the no 2:");
+  scanf("%d",&n2);
+  printf("Enter the no 3:");
+  scanf("%d",&n3);
+  sum=gresum(n1,n2,n3);
+  printf("The sum of two maximum no is :%d",sum);
+  getch();
+}
